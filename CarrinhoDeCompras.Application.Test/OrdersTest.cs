0693353bfc578934653ee8ea2d8ea5bbc912249d
Marconcilio <marconcilio@hotmail.com>
2017-11-23 16:56:17 -0200

    projeto teste

---
+++ CarrinhoDeCompras.Application.Test/OrdersTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarrinhoDeCompras.Application.Interfaces;
+using CarrinhoDeCompras.Domain.Interfaces.Services;
+using CarrinhoDeCompras.Domain.Services;
+using CarrinhoDeCompras.Domain.Entidades;
+using Moq;
+
+namespace CarrinhoDeCompras.Application.Test
+{
+    [TestClass]
+    public class OrdersTest
+    {
+        private readonly IAppServiceOrders _AppServiceOrders;
+        private readonly IAppServiceCustomers _AppServiceCustomers;
+        private readonly IAppServiceProducts _AppServiceProducts;
+        private readonly int CustomerID = 85;
+        [TestMethod]
+        public void InclirPedidos()
+        {
+            var order = new Orders()
+            {
+                CustomerID = 85,
+                Customer = "VINET",
+                EmployeeID = 5,
+                OrderDate = DateTime.Now,
+                RequiredDate = DateTime.Now,
+                ShippedDate = DateTime.Now,
+                ShipVia = 3,
+                Freight = 13,
+                ShipName = "Vins et alcools Chevalier",
+                ShipAddress = "59 rue de l'Abbaye",
+                ShipCity = "Reims",
+                ShipPostalCode = "51100",
+                ShipCountry = "France",
+            };
+
+            var mockRepo = new Mock<IAppServiceOrders>();
+            mockRepo.Setup(x => x.Add(order));
+
+
+        }
+    }
+}
---
+++ CarrinhoDeCompras.Application.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CarrinhoDeCompras.Application.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CarrinhoDeCompras.Application.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("02c40075-95ba-450e-bbf7-037aa955d99a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
